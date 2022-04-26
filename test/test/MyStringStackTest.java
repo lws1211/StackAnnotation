41cb3a449239fcd776696801ca6029f601af4078
Wan Seng Liew (NCS) <wanseng.liew@ncs.com.sg>
2022-04-26 19:21:20 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -36,10 +36,8 @@ public class MyStringStackTest extends TestCase {
 		assertTrue (stack.isEmpty());
 		stack.push (s1);
         stack.push (s2);
-        assertFalse (stack.isEmpty());
-	MyStringStack stack = new MyStringStack();
-	stack.push (s1);
-	assertTrue (stack.isEmpty());
+        assertTrue(stack.isEmpty());
+
 	}
 
 	@Test
